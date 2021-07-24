ec45862d43be5cc6ec3cd6eb6bbcefb8e5ece787
jasantosm <ingjuliansantos@gmail.com>
2021-07-24 17:18:30 -0500

    Database updated

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 db_user = os.environ["DB_USER"]
 db_password = os.environ["DB_PASSWORD"]
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@127.0.0.1:5432/ice_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@ice-db.postgres.database.azure.com:5432/ice_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
