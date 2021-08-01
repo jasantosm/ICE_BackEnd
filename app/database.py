86adf4d2329ff181db2c8a6cb0ef37c7743bba0c
jasantosm <ingjuliansantos@gmail.com>
2021-08-01 12:18:33 -0500

    Added UTF8 to SQLALCHEMY Engine

--- app/database.py
+++ app/database.py
@@ -8,7 +8,8 @@ db_password = os.environ["DB_PASSWORD"]
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@ice-db.postgres.database.azure.com:5432/ice_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, client_encoding="utf8")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
